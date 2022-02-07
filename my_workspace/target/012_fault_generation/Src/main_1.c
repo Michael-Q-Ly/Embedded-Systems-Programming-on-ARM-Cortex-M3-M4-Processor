a7e49dd92999fca8ed6d266d340ca87904bf15ca
Michael Ly <71558719+Michael-Q-Ly@users.noreply.github.com>
2022-02-07 17:41:32 -0600

    Add Example 12 (two versions): 	- fault_gen is less descriptive 	- The purpose of this exercise is to show how fault generation 	  works in the ARM Cortex M4 processor. 	- We first configure any configurable exceptions such as USFR, 	  MMFR, and BFR so that they are not forced to HFSR. 	- main.c shows an example of how to create an undefined 	  instruction (wrong opcode). To do this, we create a pointer in 	  SRAM and give it the contents 0xFFFFFFFF (undef. opcode). 	  We then create a function pointer and then call that function 	  pointer (after making the address odd to stay in THUMB ISA). 	  This causes a usage fault. 	- We then learn that C functions, when disassembled, have a 	  prologue and epilogue. This means we cannot trust the value of 	  MSP when we are debugging. We must make a PURE assembly 	  function. 	- To make a pure assembly function, we must use the 	  __attribute__( (naked)) ) attribute. When we move MSP to R0, 	  we can look into the Cortex M4 user guide and see that [R0] 	  becomes argument 1 for the callee, where the naked attribute 	  function is the caller function. We may then branch to our C 	  function and pass [R0] as a pointer. We then get the contents 	  of the stack frame: xPSR, PC, LR, R12, R3, R2, R1, and R0. We 	  just need to remember that the stack is LIFO and descending 	  memory. Thus, the base of the stack frame up starts at R0 up 	  to xPSR. 	- Note: We can check the SFRs to see if control registers have 	  been set for fault handers
    	- main_1.c:
    	- This program runs if we disable main.c in our project space.
    	  Be sure to do so.
    	- We try to do a divide by zero here and enable the
    	  divide-by-zero trap by going to the CCR (Configuration Control
    	  Register). We grab its address and store it into a pointer.
    	- Now, using the M4 user guide, we look at the CCR and see that
    	  bit 4 is the DIV_0_TRP. We set the bit.
    	- We now make a new function called fun_divide and send it two
    	  parameters, the second being zero.
    	- As soon as we try to return zero from fun_divide, we are
    	  thrown into the usage fault handler function, which uses the
    	  naked attribute.
    	- Using single-instruction stepping, we see that our assembly
    	  instructions work. MRS stores the contents of special register
    	  MSP into the register R0.
    	- We then use the branch instruction to branch to our C usage
    	  fault handler function.
    	- Before we branch, we can check the contents of the return
    	  address register (pc) and see that we indeed have a usage
    	  fault exception!
    	- We can now do normal steps and step over into each new
    	  instruction in our C usage fault handler function and obtain
    	  the contents of the stack frame.
    	- Obtaining these values is useful for debugging purposes to see
    	  where our code went wrong. We would normally check the LR and
    	  PC registers.

---
+++ my_workspace/target/012_fault_generation/Src/main_1.c
@@ -0,0 +1,105 @@
+/*
+ * main_1.c
+ *
+ *  Created on: Feb 7, 2022
+ *      Author: Micha
+ */
+
+/**
+ * Exercise:
+ * Write a program to enable all configurable fault exceptions, implement
+ * the fault exception handlers and cuase the fault by the following methods:
+ *
+ * 1. Execute an undefined instruction
+ * 2. Divide by zero.
+ * 3. Try executing an instruction from the peripheral region
+ * 4. Execute SVC inside the SVC handler
+ * 5. Execute an SVC instruction inside the interrupt handler whose
+ *    priority is the same or lesser than the SVC handler
+ */
+
+
+#include <stdint.h>
+#include <stdio.h>
+
+#if !defined(__SOFT_FP__) && defined(__ARM_FP)
+  #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
+#endif
+
+extern void initialise_monitor_handles(void) ;								// Debugger
+void HardFault_Handler( void ) ;
+void MemManage_Handler( void ) ;
+void BusFault_Handler( void ) ;
+__attribute__( (naked) ) void UsageFault_Handler( void ) ;
+void UsageFault_Handler_c( uint32_t *pBaseStackFrame ) ;
+int fun_divide( int x, int y ) ;
+
+int main( void ) {
+	initialise_monitor_handles() ;									// Debugger
+
+	// 1. Enable all configurable exceptions like usage fault, memory manage fault, and bus fault
+	uint32_t *pSHCSR = ( uint32_t* )( 0xE000ED24 ) ;						// System Handler Control & State Register
+	// Use the table to see which bits to set the usage fault, memory manage fault, and bus fault: bits 16, 17, & 18 for Cortex M4
+	*pSHCSR |= ( 1 << 16 ) ;	// Memory Manage
+	*pSHCSR |= ( 1 << 17 ) ;	// Bus Fault
+	*pSHCSR |= ( 1 << 18 ) ;	// Usage Fault
+
+	// 2. Implement the fault handlers (Done outside of main; see below)
+
+	// 3. Enable divide-by-zero trap
+	uint32_t *pCCR = ( uint32_t* )( 0xE000ED14 ) ;
+	*pCCR |= ( 1 << 4 ) ;
+
+	// 4. Attempt to divide by zero
+	fun_divide( 10, 0 ) ;
+	// 5. Analyze the fault
+
+	/* Loop forever */
+	for( ; ; ) ;
+}
+
+
+// 2. Implement the fault handlers
+void HardFault_Handler( void ) {
+	fprintf( stderr, "Exception: Hardfault\n" ) ;
+	while( 1 ) ;
+}
+
+void MemManage_Handler( void ) {
+	fprintf( stderr, "Exception: MemManage\n" ) ;
+	while( 1 ) ;
+}
+
+void BusFault_Handler( void ) {
+	fprintf( stderr, "Exception: Busfault\n" ) ;
+	while( 1 ) ;
+}
+
+__attribute__ ( (naked) ) void UsageFault_Handler( void ) {						// Caller
+	__asm ( "MRS r0, MSP" ) ;									// Contents of r0 become argument 1 for the callee
+	__asm( "B UsageFault_Handler_c" ) ;
+
+}
+
+void UsageFault_Handler_c( uint32_t *pBaseStackFrame ) {						// Callee
+	// 4. Analyze the fault
+	uint32_t *pUFSR = ( uint32_t* )( 0xE000ED2A ) ;
+	fprintf( stderr, "Exception: Usagefault\n" ) ;
+	printf( "UFSR = %lx\n", ( *pUFSR ) & 0xFFFF ) ;							// We only use the first 16 bits, so clear the lower 16 bits with an & 0xFFFF
+	printf( "pBaseStackFrame = %p\n", pBaseStackFrame ) ;
+	printf( "Value of R0 = %lx\n", pBaseStackFrame[0] ) ;
+	printf( "Value of R1 = %lx\n", pBaseStackFrame[1] ) ;
+	printf( "Value of R2 = %lx\n", pBaseStackFrame[2] ) ;
+	printf( "Value of R3 = %lx\n", pBaseStackFrame[3] ) ;
+	printf( "Value of R12 = %lx\n", pBaseStackFrame[4] ) ;
+	printf( "Value of LR = %lx\n", pBaseStackFrame[5] ) ;
+	printf( "Value of PC = %lx\n", pBaseStackFrame[6] ) ;
+	printf( "Value of xPSR = %lx\n", pBaseStackFrame[7] ) ;
+
+	while( 1 ) ;
+}
+
+int fun_divide( int x, int y ) {
+	return ( x / y ) ;
+}
+
