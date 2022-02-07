50217734b75f5e05c07ba652a1c104880e829ed8
Michael Ly <71558719+Michael-Q-Ly@users.noreply.github.com>
2022-02-07 17:41:32 -0600

    Add Example 12 (two versions):
    	- fault_gen is less descriptive
    	- The purpose of this exercise is to show how fault generation
    	  works in the ARM Cortex M4 processor.
    	- We first configure any configurable exceptions such as USFR,
    	  MMFR, and BFR so that they are not forced to HFSR.
    	- main.c shows an example of how to create an undefined
    	  instruction (wrong opcode). To do this, we create a pointer in
    	  SRAM and give it the contents 0xFFFFFFFF (undef. opcode).
    	  We then create a function pointer and then call that function
    	  pointer (after making the address odd to stay in THUMB ISA).
    	  This causes a usage fault.
    	- We then learn that C functions, when disassembled, have a
    	  prologue and epilogue. This means we cannot trust the value of
    	  MSP when we are debugging. We must make a PURE assembly
    	  function.
    	- To make a pure assembly function, we must use the
    	  __attribute__( (naked)) ) attribute. When we move MSP to R0,
    	  we can look into the Cortex M4 user guide and see that [R0]
    	  becomes argument 1 for the callee, where the naked attribute
    	  function is the caller function. We may then branch to our C
    	  function and pass [R0] as a pointer. We then get the contents
    	  of the stack frame: xPSR, PC, LR, R12, R3, R2, R1, and R0. We
    	  just need to remember that the stack is LIFO and descending
    	  memory. Thus, the base of the stack frame up starts at R0 up
    	  to xPSR.

---
+++ my_workspace/target/012_fault_generation/Debug/Src/subdir.mk
@@ -0,0 +1,46 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (9-2020-q2-update)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+<<<<<<< HEAD
+../Src/main.c \
+../Src/sysmem.c 
+
+OBJS += \
+./Src/main.o \
+./Src/sysmem.o 
+
+C_DEPS += \
+./Src/main.d \
+=======
+../Src/main_1.c \
+../Src/sysmem.c 
+
+OBJS += \
+./Src/main_1.o \
+./Src/sysmem.o 
+
+C_DEPS += \
+./Src/main_1.d \
+>>>>>>> a7e49dd (Add Example 12 (two versions):)
+./Src/sysmem.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Src/%.o: ../Src/%.c Src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DSTM32F429I_DISC1 -DSTM32 -DSTM32F429ZITx -DSTM32F4 -c -I../Inc -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+
+clean: clean-Src
+
+clean-Src:
+<<<<<<< HEAD
+	-$(RM) ./Src/main.d ./Src/main.o ./Src/sysmem.d ./Src/sysmem.o
+=======
+	-$(RM) ./Src/main_1.d ./Src/main_1.o ./Src/sysmem.d ./Src/sysmem.o
+>>>>>>> a7e49dd (Add Example 12 (two versions):)
+
+.PHONY: clean-Src
+
