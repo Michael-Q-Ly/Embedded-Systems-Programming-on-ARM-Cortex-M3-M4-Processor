9a8074fafefcd43716ea83a8494f0c7bf11a576a
Michael Ly <71558719+Michael-Q-Ly@users.noreply.github.com>
2022-02-10 23:09:18 -0600

    Example 15: Create functions to initialize the stack 	- Initialize the stack frame and create dummy variable for xPSR 	- Capture the addresses of the stack handlers

--- my_workspace/target/015_task_scheduler/Src/main_1.c
+++ my_workspace/target/015_task_scheduler/Src/main_1.c
@@ -7,25 +7,15 @@
 
 #include <stdio.h>
 #include <stdint.h>
+#include "main_1.h"
 
-/* Some stack memory calculations */
-#define SIZE_TASK_STACK			1024U
-#define SIZE_SCHEDULER_STACK		1024U
+uint32_t psp_of_tasks[MAX_TASKS] = {	T1_STACK_START
+					,	T2_STACK_START
+					,	T3_STACK_START
+					,	T4_STACK_START
+} ;
 
-#define SRAM_START			0x20000000U
-#define SIZE_SRAM			( ( 128 ) * ( 1024 ) )
-#define SRAM_END			( ( SRAM_START ) + ( SIZE_SRAM ) )
-
-#define T1_STACK_START			SRAM_END
-#define T2_STACK_START			( ( SRAM_END ) - ( 1 * SIZE_TASK_STACK ) )
-#define T3_STACK_START			( ( SRAM_END ) - ( 2 * SIZE_TASK_STACK ) )
-#define T4_STACK_START			( ( SRAM_END ) - ( 3 * SIZE_TASK_STACK ) )
-#define SCHED_STACK_START		( ( SRAM_END ) - ( 4 * SIZE_TASK_STACK ) )
-
-#define TICK_HZ				1000U
-
-#define HSI_CLOCK			16000000U
-#define SYSTICK_TIM_CLK			HSI_CLOCK
+uint32_t task_handlers[MAX_TASKS] ;
 
 /* Function Prototypes */
 extern void initialise_monitor_handles( void ) ;								// Debugger
@@ -34,6 +24,8 @@ void task2_handler( void ) ;                                                    
 void task3_handler( void ) ;                                                    				// This is task3
 void task4_handler( void ) ;                                                    				// This is task4
 
+__attribute__( (naked) ) void init_scheduler_stack( uint32_t sched_top_of_stack ) ;
+void init_tasks_stack( void ) ;
 void init_systick_timer( uint32_t tick_hz ) ;
 void SysTick_Handler( void ) ;											// This is our scheduler--what we use to do context switching
 
@@ -41,10 +33,25 @@ int main(void)
 {
 	initialise_monitor_handles() ;                                          				// Debugger
 
+	enable_processor_faults() ;
+
+	// Initialize the scheduler stack (which is MSP)
+	init_scheduler_stack( SCHED_STACK_START ) ;
+	
+	// Array-filling; capturing addresses of different task handlers
+	task_handlers[0] = ( uint32_t) ( task1_handler ) ;
+	task_handlers[1] = ( uint32_t) ( task2_handler ) ;
+	task_handlers[2] = ( uint32_t) ( task3_handler ) ;
+	task_handlers[3] = ( uint32_t) ( task4_handler ) ;
+
+	// Task stack initialization to store the dummy frames  
+	init_tasks_stack() ;
+
+	// Generate SysTick timer exception
 	init_systick_timer( TICK_HZ ) ;
 
 	/* Loop forever */
-		for(;;);
+	for(;;);
 }
 
 
@@ -99,6 +106,38 @@ void init_systick_timer( uint32_t tick_hz ) {
 
 	// Enable the SysTick
 	*pSCSR |= ( 1 << 0 ) ;										// Enables the counter
+}
+
+__attribute__( (naked) ) void init_scheduler_stack( uint32_t sched_top_of_stack ) {
+//	__asm volatile ( "MSR MSP, R0" ) ;
+	__asm volatile( "MSR MSP, %0" : : "r" ( sched_top_of_stack ) : ) ;				// The GCC inline with C variable equivalent of above
+	__asm volatile ( "BX LR" ) ;									// BX copies the value of LR into PC, where LR contains the return address
+}
+
+void init_tasks_stack( void ) {
+
+	uint32_t *pPSP ;
+	for (int i = 0 ; i < MAX_TASKS ; i++ ) {
+		pPSP = ( uint32_t* )( psp_of_tasks[i] ) ;
+		
+		pPSP-- ;										// xPSR
+		*pPSP = DUMMY_XPSR ;									// 0x01000000
+
+		pPSP-- ;										// PC
+		*pPSP = task_handlers[i] ;
+
+		pPSP-- ;										// LR
+		*pPSP = 0xFFFFFFFD ;
+
+		// Registers R0 through R12:
+		for ( int j = 0 ; j < 13 ; j++ ) {
+			pPSP-- ;
+			*pPSP = 0 ;
+		}
+
+		// Preserve the value of PSP once you have loaded the Stack Frame.
+		psp_of_tasks[i] = ( uint32_t)( pPSP ) ;
+	}
 }
 
 void SysTick_Handler( void ) {
